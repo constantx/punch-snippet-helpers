08d5eaa08e681700f8249562ae8c3b7cef162ff8
Truong Nguyen <constantx@gmail.com>
2013-10-06 13:52:56 -0700

    fix jslint, add typekit snippet

--- test/module_test.js
+++ test/module_test.js
@@ -1,4 +1,4 @@
-/*jslint indent:2, node:true, nomen:true, unparam: true */
+/*jslint indent:2, node:true, nomen:true, unparam:true */
 /*global require:false */
 
 (function () {
@@ -13,20 +13,37 @@
       ga: {
         property_id: "UA-XXXXXXX-XX",
         domain: "google.com"
+      },
+      typekit: {
+        id: "xxxxxx"
       }
     }
   };
 
-  exports.testModule = function(test) {
+  exports.google_analytics_js = function (test) {
 
     PunchHelper
       .setup(mockConfig)
-      .get(null, null, null, function(err, res, res_options, last_modified) {
+      .get(null, null, null, function (err, res, res_options, last_modified) {
         if (!err) {
-          res_options = res_options;
-          last_modified = last_modified;
-
           test.equal(res.tag.google_analytics_js(), "<script type='text/javascript'>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-XXXXXXX-XX', 'google.com');ga('send', 'pageview');</script>");
+
+          // how many test to expect
+          test.expect(1);
+
+          // done test
+          test.done();
+        }
+      });
+  };
+
+  exports.typekit = function (test) {
+
+    PunchHelper
+      .setup(mockConfig)
+      .get(null, null, null, function (err, res, res_options, last_modified) {
+        if (!err) {
+          test.equal(res.tag.typekit(), "<script type='text/javascript' src='//use.typekit.net/xxxxxx.js'></script><script type='text/javascript'>try{Typekit.load();}catch(e){}</script>");
 
           // how many test to expect
           test.expect(1);
